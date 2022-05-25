f3a03a7a8e1cd4ccf918c80ee4cedc751c2c58ca
zohirulislam1 <codershahin84@gmail.com>
2022-05-25 10:51:06 +0600

    new commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 app.get('/', (req, res) => {
-    res.send('Hello Assignment_12')
+    res.send('Hello Assignment_12-Finish')
   })
   
 
