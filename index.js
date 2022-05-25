973db0d9505686cc4b5400597de4915e094bc99a
zohirulislam1 <codershahin84@gmail.com>
2022-05-25 11:44:08 +0600

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port =process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.g8jgs.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g8jgs.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
